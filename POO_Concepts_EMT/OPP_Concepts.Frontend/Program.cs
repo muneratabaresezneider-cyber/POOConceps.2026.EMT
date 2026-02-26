dc3f1d9b771e47ee8fa95ff436e7cd506089edb4
ESNEIDER MUNERA TABARES <esneidermunera1158132@correo.itm.edu.co>
2026-02-26 15:06:07 -0500

    Complete

---
+++ POO_Concepts_EMT/OPP_Concepts.Frontend/Program.cs
@@ -0,0 +1,18 @@
+﻿using OPP_Comcepts.Backend;
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(2026, 2, 28);
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
